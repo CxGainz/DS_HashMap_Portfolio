d39419d1d6b974752c87eee681a905ea4dd4a700
CxGainz <118799186+CxGainz@users.noreply.github.com>
2023-08-13 20:35:43 -0700

    Update hash_map_oa.py

--- HashMapPortfolio/hash_map_oa.py
+++ HashMapPortfolio/hash_map_oa.py
@@ -182,7 +182,9 @@ class HashMap:
 
     def empty_buckets(self) -> int:
         """
-        TODO: Write this implementation
+        This is the imitated version from hash_map_sc.py version of the empty_buckets method where we use a for loop, 
+        we can instead use self.capacity-self.size for open addressing in this method. I use a for loop to double 
+        check my updates from all my method implementations work correctly.
         """
         empty_bucket = 0
         for i in range(self.buckets.length()):
